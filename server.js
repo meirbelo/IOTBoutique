bdbd3abceacbe3de6839731bc15911ce7fc714db
meirbelo <90623098+meirbelo@users.noreply.github.com>
2025-01-31 12:32:42 +0100

    import all routes

--- server.js
+++ server.js
@@ -1,35 +1,33 @@
+require('dotenv').config();
 const express = require('express')
 const cors = require("cors");
 const path = require('path')
+const mongoose = require('mongoose')
+const db = mongoose.connection
+const cookieParser = require('cookie-parser');
 const app  = express()
 const nconf = require('./config/nconf')
 
 const port = nconf.get("port")
 const environment = nconf.get("environment")
 
-var corsOptions = {
-    origin: "http://localhost:8081"
-  };
-  
   app.set('view engine', 'ejs');
   app.set('views', './views'); 
   
-  // parse requests of content-type - application/json
+  app.use(cookieParser());
   app.use(express.json());
-  
-  // parse requests of content-type - application/x-www-form-urlencoded
   app.use(express.urlencoded({ extended: true }));
-  
   app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-app.get('/' , (req ,res) =>  {
-    res.send('Hello ')
-})
 require("./routes/account.routes")(app);
-console.log(app)
+require("./routes/member.routes")(app);
+require("./routes/shop.routes")(app);
 
+   mongoose.connect(process.env.DATABASE_URL)
+   .then(() => console.log('Connected to Database'))
+   .catch((error) => console.error('Database connection error:', error));
+ 
+   
 app.listen(port, () => {
   
     console.log(`Application listening on port ${port} in ${environment} mode.`);
